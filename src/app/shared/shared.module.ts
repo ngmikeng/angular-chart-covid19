66196acd67384aa2fd419414024fb5a0584fc1c7
Minh Nguyen <ngmikeng@gmail.com>
2022-01-10 00:25:57 +0700

    Fix table for paging

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -4,11 +4,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { TranslateModule } from '@ngx-translate/core';
 import { MatCardModule } from '@angular/material/card';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatAutocompleteModule} from '@angular/material/autocomplete';
-import { MatFormFieldModule} from '@angular/material/form-field';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
@@ -28,6 +29,7 @@ const LIB_MODULES = [
   MatInputModule,
   MatSelectModule,
   MatTableModule,
+  MatPaginatorModule,
   MatDividerModule,
   MatButtonModule,
   MatButtonToggleModule,
